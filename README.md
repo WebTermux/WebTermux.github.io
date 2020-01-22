3c4c83cc2fadb5295cb2b7a9bad678735e8e9ed6
Raj Gh <59818709+Developer-Raj@users.noreply.github.com>
2020-01-22 19:20:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# WebTermux.github.io
-A JavaScript based web-terminal, where you can find all stuff in one place making your life easier, efficient &amp; productive.
+# WEB-TERMINAL ![site-status](https://img.shields.io/badge/status-up-brightgreen) ![build-version](https://img.shields.io/badge/build-beta_v1.0.1a-brightgreen)
+
+A JavaScript based web-terminal, where you can find all stuff in one place making your life easier, efficient & productive. 
+
+> **Note** if you encounter any bug please let us know on here: [issue](https://github.com/Developer-Raj/web-terminal/issues)
+
+
+***
+
+## Development Timeline
+
+### **What's New!**
+>- Added option to clear terminal.
+>- Added option to show alert message.
+>- Added option to google search.
+>- Added new url option.
+
+### **Bug Fix's**
+>- Hurray Bug free
+
+***
+## Usage
+
+Type `.help` in terminal to print all available commands.
+
+- `.clear` option will clear the terminal.
+- `.alert <value>` option will alert a given value. | .alert hello world
+- `.google <value>` option will search your given value in google | .goole youtube
+- `.url <value.com>` option will open a new-tab with your given value.com | .url example.com
+- `.cal <value>` option will calculate a given value | .cal 1+2 //3
+***
---
+++ assets/scripts/main.js
@@ -0,0 +1,56 @@
+function $(a) {return document.querySelector(a);}
+
+var input = $(".console"),
+    display = $(".display");
+    
+    display.onclick = function() {
+        input.focus();
+    };
+    
+display.innerText = "Welcome to JavaScript Web-Terminal App\nAuthor: Raj Ghosh\nVersion: beta v1.0.1a\n\n";
+
+input.onkeyup = function(e) {
+    var value = input.value;
+
+    
+
+    if(e.keyCode===13){
+
+        display.innerText += ">> " + value + "\n";
+        input.value = "";
+        
+        if(value.match(/\.clear/g)) {
+            display.innerText = "Welcome to JavaScript Web-Terminal App\nAuthor: Raj Ghosh\nVersion: beta v1.0.1a\n\n>> " + "\n";
+
+        } else if(value.match(/\.help/g)) {
+
+            display.innerText += "\tThese are the all available commands you can use.\n\n.help - Print all available commands.\n.clear - Clear Terminal.\n.alert <value> - To show alert message\n.google <value> - To search google from the terminal\n.cal <value> - To calculate a given value\n.url <value.com> - To open a url in new window"+"\n\n";
+
+        } else if(value.match(/\.alert /g)) {
+
+            var message = value.replace(/\.alert /g, "");
+            alert(message);
+
+        } else if(value.match(/\.cal /g)) {
+            var calCulate = value.replace(/\.cal /g, "");
+            display.innerText += eval(calCulate)+"\n";
+
+        } else if(value.match(/\.google /g)) {
+            
+            var search = value.replace(/\.google /g, "https://www.google.com/search?q=");
+            window.open(search, "_blank");
+
+        } else if(value.match(/\.url /g)){
+            var url = value.replace(/\.url /g, "https://");
+            window.open(url, "_blank");
+        } else {
+            
+            if(value === ""){
+                display.innerText += "";
+            }else {
+                display.innerText += "{" + value + "} is not recognised by our system, type .help to print all available commands\n";
+            }
+            
+        }
+    }
+};
