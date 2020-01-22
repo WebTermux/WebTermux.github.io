8a18f1bd0f0068139bc76741d7476796f1201139
Raj Gh <59818709+Developer-Raj@users.noreply.github.com>
2020-01-22 22:58:35 +0530

    Add files via upload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,40 +7,39 @@ var input = $(".console"),
         input.focus();
     };
     
-display.innerText = "Welcome to WebTermux App\nAuthor: Raj Ghosh\nVersion: beta v0.0.1a\n\n";
+display.innerText = "Welcome to WebTermux App\nAuthor: Raj Ghosh\nVersion: beta v0.0.1bfm\n\n";
 
-input.onkeyup = function(e) {
+input.onkeydown = function(e) {
     var value = input.value;
 
     
 
     if(e.keyCode===13){
-
         display.innerText += ">> " + value + "\n";
         input.value = "";
-        
-        if(value.match(/\.clear/g)) {
-            display.innerText = "Welcome to WebTermux App\nAuthor: Raj Ghosh\nVersion: beta v0.0.1a\n\n>> " + "\n";
+        display.scrollBy(0, 9999999);
+        if(value.match(/\.clear/g)) { //Clear Function
+            display.innerText = "Welcome to WebTermux App\nAuthor: Raj Ghosh\nVersion: beta v0.0.1bfm\n\n>> " + "\n";
 
-        } else if(value.match(/\.help/g)) {
+        } else if(value.match(/\.help/g)) { //Help Function
 
             display.innerText += "\tThese are the all available commands you can use.\n\n.help - Print all available commands.\n.clear - Clear Terminal.\n.alert <value> - To show alert message\n.google <value> - To search google from the terminal\n.cal <value> - To calculate a given value\n.url <value.com> - To open a url in new window"+"\n\n";
 
-        } else if(value.match(/\.alert /g)) {
+        } else if(value.match(/\.alert /g)) { //Alert Function
 
             var message = value.replace(/\.alert /g, "");
             alert(message);
 
-        } else if(value.match(/\.cal /g)) {
+        } else if(value.match(/\.cal /g)) { //Cal Function
             var calCulate = value.replace(/\.cal /g, "");
             display.innerText += eval(calCulate)+"\n";
 
-        } else if(value.match(/\.google /g)) {
+        } else if(value.match(/\.google /g)) { //Google Function
             
             var search = value.replace(/\.google /g, "https://www.google.com/search?q=");
             window.open(search, "_blank");
 
-        } else if(value.match(/\.url /g)){
+        } else if(value.match(/\.url /g)){ //Url Function
             var url = value.replace(/\.url /g, "https://");
             window.open(url, "_blank");
         } else {
